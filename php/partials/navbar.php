e91087dfc6d3a0d62c794a557f8f46a411d028e6
cwong2015 <sirhc8002@gmail.com>
2018-05-05 17:21:27 -0400

    minor style change to display login/register links; added some useful TODO comments for server logic

--- php/partials/navbar.php
+++ php/partials/navbar.php
@@ -1,15 +1,16 @@
 <div class="navbar navbar-light bg-light">
     <a class="navbar-brand" href="http://localhost/wayfare/php/index.php">Wayfare</a>
     <ul class="nav navbar-nav navbar-right">
-        <li>
+        <span>
+            <!-- if (user.loggedIn):-->
             <a href="http://localhost/wayfare/php/login.php">
                 Login
             </a>
-        </li>
-        <li>    
             <a href="http://localhost/wayfare/php/registration.php">
                 Register
             </a>
-        </li>
+            <!-- else: -->
+            <!-- display link to Account instead of Login/Register links -->
+        </span>
     </ul>
 </div>
