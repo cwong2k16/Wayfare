ef0a6a98e334a5f2cd88789bb6a5fff6faf2be76
cwong2015 <sirhc8002@gmail.com>
2018-05-06 15:54:51 -0400

    added datepicker, source and destination drop down data live search, with Jackie Wang

--- php/index.php
+++ php/index.php
@@ -10,5 +10,10 @@
             include '/partials/search.php';
             include '/partials/footer.php';
         ?>
+          <script src="../js/jquery.min.js"></script>
+          <script src="../js/bootstrap.min.js"></script>
+          <script src="../js/bootstrap-select.min.js"></script>
+          <script src="../js/bootstrap-datapicker.js"></script>
+          <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
     </body>
 </html>
--- php/partials/head.php
+++ php/partials/head.php
@@ -6,4 +6,19 @@
         <link rel="stylesheet" type="text/css" href="../css/styles.css">
         <script src="../js/jquery.min.js"></script>
         <script src="../js/script.js"></script>
+        <link href="../css/bootstrap.min.css" rel="stylesheet"/>
+        <link rel="stylesheet" href="../css/bootstrap-select.min.css" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css"/>
+        <script>
+    $(document).ready(function(){
+        var date_input=$('input[name="date"]'); //our date input has the name "date"
+        var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
+        date_input.datepicker({
+            format: 'mm/dd/yyyy',
+            container: container,
+            todayHighlight: true,
+            autoclose: true,
+        })
+    })
+</script>
 </head>
