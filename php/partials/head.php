d90b96f2e749ad3b892dd61ae3b14d38808a7952
cwong2015 <sirhc8002@gmail.com>
2018-05-06 17:18:53 -0400

    added radio buttons for various travel types

--- php/partials/head.php
+++ php/partials/head.php
@@ -10,15 +10,15 @@
         <link rel="stylesheet" href="../css/bootstrap-select.min.css" />
         <link rel="stylesheet" href="../css/bootstrap-datepicker3.css"/>
         <script>
-    $(document).ready(function(){
-        var date_input=$('input[name="date"]'); //our date input has the name "date"
-        var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
-        date_input.datepicker({
-            format: 'mm/dd/yyyy',
-            container: container,
-            todayHighlight: true,
-            autoclose: true,
+        $(document).ready(function(){
+            var date_input=$('input[name="date"]'); //our date input has the name "date"
+            var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
+            date_input.datepicker({
+                format: 'mm/dd/yyyy',
+                container: container,
+                todayHighlight: true,
+                autoclose: true,
+            })
         })
-    })
 </script>
 </head>
--- php/partials/search.php
+++ php/partials/search.php
@@ -1,6 +1,12 @@
 <div id = "booking" style="text-align:center">
     <div style="padding:5px">
-  <span class="glyphicon glyphicon-search"></span>
+        <form id = "travelTypes" style = "padding-bottom: 15px" action="">
+            <input type="radio" name="travelType" value="flight"> Flight </input>
+            <input type="radio" name="travelType" value="bus"> Bus </input>
+            <input type="radio" name="travelType" value="car"> Car </input>
+            <input type="radio" name="travelType" value="cruise"> Cruise </input>
+        </form>
+
         <select id = "source" class="selectpicker" placeholder="Source.." data-live-search="true">
             <option value="" disabled selected> Select your source... </option>
             <option>Boston, MA</option>
@@ -22,13 +28,13 @@
             <option>Stony Brook, NY</option>
         </select>
     </div>
-    <form id = "dateSource" style="width:50%" class="center-elements" method="post">
+    <form id = "dateSource" style="width:10%" class="center-elements" method="post">
     <div class="form-group"> <!-- Date input -->
         <label class="control-label" for="date">Source Date</label>
         <input class="form-control" id="date" name="date" placeholder="MM/DD/YYY" type="text"/>
     </div>
     </form>
-    <form id = "dateDestination" style="width:50%" class="center-elements" method="post">
+    <form id = "dateDestination" style="width:10%" class="center-elements" method="post">
     <div class="form-group"> <!-- Date input -->
         <label class="control-label" for="date">Destination Date</label>
         <input class="form-control" id="date" name="date" placeholder="MM/DD/YYY" type="text"/>
