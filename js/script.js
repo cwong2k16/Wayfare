1a88ef7221fbbb5800e9bb070a695a3a2c778f98
cwong2015 <sirhc8002@gmail.com>
2018-05-06 19:19:58 -0400

    starting to hook up front end with back end using AJAX calls

--- js/script.js
+++ js/script.js
@@ -1,3 +1,42 @@
 $(document).ready(function(){
     $("#banner").fadeIn(1000);
 });
+
+function signUp() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "./php/server_code/addpassenger.php", true);
+    xhr.setRequestHeader("Content-type", "application/json");
+    xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                    var json = JSON.parse(xhr.responseText);
+                    console.log(json);
+                    if (json.status == OK) {
+                            alert("success");
+                    } else {
+                            alert("failure");
+                    }
+            }
+    }
+    var firstName = getValue('firstName');
+    var lastName = getValue('lastName');
+    var email = getValue('email');
+    var zipcode = getValue('zipcode');
+    var street = getValue('street');
+    var password = getValue('password');
+    var password2 = getValue('password2');
+    var birthdate = getValue('birthdate');
+    birthdate = toUnix(birthdate);
+
+    var data = JSON.stringify({"firstName": firstName, "lastName": lastName,  "email": email,
+                               "zipcode":zipcode, "street": street, "password":password, "password2":password2,
+                               "birthdate": birthdate});
+    xhr.send(data);
+}
+
+function getValue(attr){
+    return document.getElementById(attr).value();
+}
+
+function toUnix(birthdate){
+    
+}
--- php/partials/registrationForm.php
+++ php/partials/registrationForm.php
@@ -1,5 +1,5 @@
 <div class="center-elements">
-    <form>
+    <form method="signUp()">
         <div class="form-group">
             <h3 class="form-signup-heading">Please register for an account</h3>
         </div>
@@ -9,29 +9,41 @@
         <div class="form-group">
             <input id="lastName" class="form-control" placeholder="Last Name" required type="password">
         </div>
-        <div style = "padding-bottom: 10px" class="form-group">
+        <div class="form-group">
             <select id = "gender" class="selectpicker" placeholder="Enter gender" data-live-search="true">
                 <option value="" disabled selected> Select your gender... </option>
                 <option>Male</option>
                 <option>Female</option>
                 <option>Other</option>
             </select>
-            <input class="form-control" id="date" name="date" placeholder="Birthdate (MM/DD/YYYY)" type="text"/>
+        </div>
+        <div style = "padding-bottom: 0px" class="form-group">
+            <input class="form-control" id="birthdate" name="date" placeholder="Birthdate (MM/DD/YYYY)" type="text"/>
         </div>
         <div class="form-group">
-            <input id="signupEmail" class="form-control" placeholder="Email address" required type="email">
+            <input id="email" class="form-control" placeholder="Email address" required type="email">
         </div>
         <div class="form-group">
-            <input id="signupStreet" class="form-control" placeholder="Street address" required type="email">
+            <input id="street" class="form-control" placeholder="Street address" required type="email">
         </div>
         <div class="form-group">
-            <input id="signupEmail" class="form-control" placeholder="Email address" required type="email">
+            <select id = "state" class="selectpicker" placeholder="State.." data-live-search="true">
+                <option value="" disabled selected> Select your state... </option>
+                <option>NY</option>
+                <option>TX</option>
+                <option>CA</option>
+                <option>NJ</option>
+                <option>FL</option>
+            </select>
         </div>
         <div class="form-group">
-            <input id="signupPassword" class="form-control" placeholder="Password" required type="password">
+            <input id="zipcode" class="form-control" placeholder="Zipcode" required type="email">
         </div>
         <div class="form-group">
-            <input id="signupPassword2" class="form-control" placeholder="Reenter password" required type="password">
+            <input id="password" class="form-control" placeholder="Password" required type="password">
+        </div>
+        <div class="form-group">
+            <input id="password2" class="form-control" placeholder="Reenter password" required type="password">
         </div>
         <div class="form-group">
             <button class="btn btn-lg btn-primary btn-block" type="submit">Register</button>
