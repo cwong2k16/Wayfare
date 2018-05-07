d839094037f5d379ccb4a938e28304bb0f47bd7b
cwong2015 <sirhc8002@gmail.com>
2018-05-06 20:49:03 -0400

    fixed padding on login form using bootstrap form-group class

--- php/partials/loginForm.php
+++ php/partials/loginForm.php
@@ -1,8 +1,14 @@
 <div class="center-elements">
     <form class="form-signin">
         <h3 class="form-signin-heading">Please log in</h3>
-        <input id="loginEmail" class="form-control" placeholder="Email address" required type="email">
-        <input id="loginPassword" class="form-control" placeholder="Password" required type="password">
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+        <div class="form-group">
+            <input id="loginEmail" class="form-control" placeholder="Email address" required type="email">
+        </div>
+        <div class="form-group">
+            <input id="loginPassword" class="form-control" placeholder="Password" required type="password">
+        </div>
+        <div class="form-group">
+            <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+        </div>
     </form>
 </div>
