c74b363349686d87393e0e68274ff4e48e84e943
cwong2015 <sirhc8002@gmail.com>
2018-05-07 14:05:05 -0400

    updated links for navbar

--- php/partials/navbar.php
+++ php/partials/navbar.php
@@ -2,15 +2,15 @@
 <nav class="navbar navbar-light bg-light">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="http://localhost/wayfare/php/index.php">Wayfare</a>
+      <a class="navbar-brand" href="http://130.245.171.91/php/index.php">Wayfare</a>
     </div>
     <ul class="nav navbar-nav navbar-right">
         <li>
             <span>
-                <a href="http://localhost/wayfare/php/login.php">
+                <a href="http://130.245.171.91/php/login.php">
                     Login
                 </a>
-                <a href="http://localhost/wayfare/php/registration.php">
+                <a href="http://130.245.171.91/php/registration.php">
                     Register
                 </a>
             </span>
