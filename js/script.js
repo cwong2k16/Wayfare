b5ad51c97834cc33ae27dbf6c7abde592234197f
cwong2015 <sirhc8002@gmail.com>
2018-05-07 14:41:20 -0400

    committing changes for merge

--- js/script.js
+++ js/script.js
@@ -56,6 +56,7 @@ function displayTravelOptions(){
     xhr.onreadystatechange = function() {
             if (xhr.readyState === 4 && xhr.status === 200) {
                     var json = JSON.parse(xhr.responseText);
+                    console.log(json);
                     if (json.status == OK) {
                             alert("success");
                     } else {
